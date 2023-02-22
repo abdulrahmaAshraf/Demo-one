10e5a458e306f2ddf8393b36b744c069283d063b
Abdelrahman Ashraf <abdelrahman.asharaf@kortobaa.com>
2023-02-22 16:05:15 +0200

    add auth decorator for admin-users controller

--- src/controllers/admins/admin-users.controller.ts
+++ src/controllers/admins/admin-users.controller.ts
@@ -1,3 +1,4 @@
+import { authenticate } from '@loopback/authentication';
 import {
   Count,
   CountSchema,
@@ -20,6 +21,7 @@ import {
 import {User} from '../../models';
 import {UserRepository} from '../../repositories';
 
+@authenticate('jwt')
 export class AdminUsersController {
   constructor(
     @repository(UserRepository)
